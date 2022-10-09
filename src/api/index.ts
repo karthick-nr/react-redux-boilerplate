7e8bbb9202faf6ec2aecc774e99bf00e45214071
Karthick N R <karthicknr90@gmail.com>
2022-10-09 21:47:43 +0200

    Added base url for ui and api and also changes to dependencies or dev dependencies

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,4 @@
-export const getBaseUrl = () => {
+export const getUIBaseUrl = () => process.env.REACT_APP_UI_BASE_URL;
 
-}
-
-
+export const getAPIBaseUrl = () => process.env.REACT_APP_API_BASE_URL;
 
